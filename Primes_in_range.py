9db53f9e6706228b3120382cd51acfc79400ebe1
Mkmounika <98087777+Mkmounika@users.noreply.github.com>
2022-05-14 11:55:05 +0530

    def fun(n):     if(n<2):         return False     for i in range(2,int(n**0.5)+1):         if(n%i==0):             return False     return True
    m=int(input())
    n=int(input())
    c=0
    for i in range (m,n+1):
        if(fun(i)):
            c+=1
    print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,16 @@
+def fun(n):
+    if(n<2):
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if(n%i==0):
+            return False
+    return True
+
+
+m=int(input())
+n=int(input())
+c=0
+for i in range (m,n+1):
+    if(fun(i)):
+        c+=1
+print(c)
