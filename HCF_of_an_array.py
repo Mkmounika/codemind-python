bff0ee8fbbf526bf37b209765c1917b1a52630e9
Mkmounika <98087777+Mkmounika@users.noreply.github.com>
2022-07-20 17:16:09 +0530

    n=int(input()) a=list(map(int,input().split())) m=100000 for i in range (n):     if(m>a[i]):         m=a[i] for i in range(1,m+1):     c=0     for j in range(0,n):         if(a[j]%i==0):             c+=1     if(c==n):         hcf=i print(hcf)

---
+++ HCF_of_an_array.py
@@ -0,0 +1,16 @@
+n=int(input())
+a=list(map(int,input().split()))
+m=100000
+for i in range (n):
+    if(m>a[i]):
+        m=a[i]
+for i in range(1,m+1):
+    c=0
+    for j in range(0,n):
+        if(a[j]%i==0):
+            c+=1
+    if(c==n):
+        hcf=i
+print(hcf)
+
+
